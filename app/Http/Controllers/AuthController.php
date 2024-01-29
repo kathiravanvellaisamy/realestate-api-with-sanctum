c3383eff3d12498e55fb72502a13f605ef53bfce
kathiravan vellaisamy <kathiravanvellaisamy@gmail.com>
2024-01-29 08:41:54 +0530

    Added Logout & token expired functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,6 +47,11 @@ class AuthController extends Controller
     }
 
     public function logout(){
-        return response()->json('Logout Successfully!');
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => "You are logged out and your token has been deleted!"
+        ]);
+
     }
 }
